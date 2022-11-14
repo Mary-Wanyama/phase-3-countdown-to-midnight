07e7e18048af7e34ea542dccf36088f80d409fbb
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-14 11:59:23 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown t
+    
+    while t >= 1
+        puts "#{t} SECOND(S)!"
+        t -= 1
+    end
+
+    'HAPPY NEW YEAR!'
+
+end
+
+
+def countdown_with_sleep t
+
+    while t >= 1
+        puts "#{t} SECOND(S)!"
+        t -= 1
+        sleep 5
+    end
+
 end
